492ad7032ea3ea5bc0cf12758f15deebd2230c84
vterdunov <mail2slick@gmail.com>
2021-03-05 18:01:28 +0300

    use relative path

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,16 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/vterdunov/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/vterdunov/.fzf/bin"
+if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/vterdunov/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/vterdunov/.fzf/shell/key-bindings.zsh"
+source "${HOME}/.fzf/shell/key-bindings.zsh"
 bindkey -r '^T'
 # bindkey -r '^D'
 bindkey '^F' fzf-file-widget
--- .zshrc
+++ .zshrc
@@ -1,4 +1,4 @@
-export ZSH="/home/vterdunov/.oh-my-zsh"
+export ZSH="${HOME}/.oh-my-zsh"
 ZSH_THEME="vterdunov"
 
 plugins=(git autojump direnv kubectl helm docker docker-compose)
